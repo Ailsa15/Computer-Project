86077e3b1978133e9769184e93234814e88827d7
Ailsa15 <ailsa.scott0905@gmail.com>
2025-10-28 12:06:52 +0000

    Update Practice 2.py

--- Practice 2.py
+++ Practice 2.py
@@ -9,7 +9,6 @@ def system(state, r, l, m_e, E, a):
     dvdr = l*(l+1)*u/(r**2) - 2*m_e*(E+a/r) * u         
     return [dudr, dvdr]
 
-
 initial_conditions = [0, 1]
 r = np.linspace(0.1, 2000, 10000)
 l=0

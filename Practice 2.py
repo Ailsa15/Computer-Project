a852a27454000f7b3c71c09898ef616ec5c6185b
Ailsa15 <ailsa.scott0905@gmail.com>
2025-10-28 15:31:30 +0000

    Update Practice 2.py

--- Practice 2.py
+++ Practice 2.py
@@ -9,27 +9,53 @@ def system(state, r, l, m_e, E, a):
     dvdr = l*(l+1)*u/(r**2) - 2*m_e*(E+a/r) * u         
     return [dudr, dvdr]
 
+def CalculateNodes(x,y):
+    cross = 0
+    for i in range(1,len(y)):
+        if y[i-1]*y[i]<0:
+            cross += 1
+    return cross
+
+def NewEnergy(E1, E2, E3, Node1, Node2, Node3):
+    if Node1 == Node2:
+        E1 = E2
+        E2 = (E1 + E3)/2 
+    else:
+        E3 = E2
+        E2 = (E1 + E3)/2
+    return E1, E2, E3
+
+def Solve(initial_conditions, r, l, m_e, E1, E2, E3, a):
+    solution = odeint(system, initial_conditions, r, args=(l, m_e, E1, a))
+    solution1 = odeint(system, initial_conditions, r, args=(l, m_e, E2, a))
+    solution2 = odeint(system, initial_conditions, r, args=(l, m_e, E3, a))
+
+    u, v = solution.T
+    u1, v1 = solution1.T    
+    u2, v2 = solution2.T
+
+    return u, v, u1, v1, u2, v2
+    
 initial_conditions = [0, 1]
-r = np.linspace(0.1, 2000, 10000)
+r = np.linspace(0.1, 2500, 10000)
 l=0
-m_e=0.511
+m_e=0.511  
 
-#def CalculateEnergy:
-    
-    
-
-E=-13.6E-6
+E1=-16E-6
 a= 1/137
-E1 = -8E-6
-E2 = -14E-6
+E3 = -6E-6
+E2 = (E1+E3)/2
 
-solution = odeint(system, initial_conditions, r, args=(l, m_e, E, a))
-solution1 = odeint(system, initial_conditions, r, args=(l, m_e, E1, a))
-solution2 = odeint(system, initial_conditions, r, args=(l, m_e, E2, a))
+for i in range(500):
+    u, v, u1, v1, u2, v2 = Solve(initial_conditions, r, l, m_e, E1, E2, E3, a)
+    Node1 = (CalculateNodes(u, v))
+    Node2 = (CalculateNodes(u1, v1))
+    Node3 = (CalculateNodes(u2, v2))
+    E1, E2, E3 = NewEnergy(E1, E2, E3, Node1, Node2, Node3)
+    
+print(E2)
 
-u, v = solution.T
-u1, v1 = solution1.T    
-u2, v2 = solution2.T    
+
 
 plt.figure(figsize=(10, 5))
 plt.plot(r, u, label='x(t)', color='blue')
@@ -39,7 +65,7 @@ plt.plot(r, u2, label='x(t)', color='red')
 plt.title('Solution of Coupled First-Order Differential Equations')
 plt.xlabel('r')
 plt.ylabel('Values of x and y')
-plt.legend()
+#plt.legend()
 plt.grid()
 plt.show()
 

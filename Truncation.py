5d8cd2337ba313ed0d152a86d33bb1c57e457976
Ailsa15 <ailsa.scott0905@gmail.com>
2025-11-12 09:56:21 +0000

    Create Truncation.py

---
+++ Truncation.py
@@ -0,0 +1,108 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from scipy.integrate import simpson
+
+def system(state, r, l, m_e, E, a, n):
+    u, v = state  
+    dudr = v       
+    dvdr = l*(l+1)*u/(r**2) - 2*m_e*(E+a/r) * u   
+    return [dudr, dvdr]
+
+def CalculateInitialEnergy(n):
+    E1 = -1.36E-5/n**2+2E-7
+    E3 = -1.36E-5/n**2-2E-7
+    E2 = (E1 + E3)/2
+    return E1, E2, E3
+
+def CalculateNodes(x,y):
+    cross = 0
+    for i in range(1,len(y)):
+        if y[i-1]*y[i]<0:
+            cross += 1
+    return cross
+
+def CalculateTurningPoints(v):
+    count = 0
+    for i in range(len(v)-1):
+        if v[i]*v[i+1]<0:
+            count += 1
+    return count
+
+def NewEnergy(E1, E2, E3, Node1, Node2, Node3, Count1, Count2, Count3):
+    if Node1 == Node2 and Count1 == Count2:
+        E1 = E2
+        E2 = (E1 + E3)/2 
+    else:
+        E3 = E2
+        E2 = (E1 + E3)/2
+    return E1, E2, E3
+
+def Solve(initial_conditions, r, l, m_e, E1, E2, E3, a, n):
+    solution = odeint(system, initial_conditions, r, args=(l, m_e, E1, a, n))
+    solution1 = odeint(system, initial_conditions, r, args=(l, m_e, E2, a, n))
+    solution2 = odeint(system, initial_conditions, r, args=(l, m_e, E3, a, n))
+    u, v = solution.T
+    u1, v1 = solution1.T    
+    u2, v2 = solution2.T
+    return u, v, u1, v1, u2, v2
+
+def normalisation(u, r):
+    normalisation = simpson(u**2, r)
+    u_normalised = u/np.sqrt(normalisation)
+    u_squared = u_normalised**2
+    return u_squared
+    
+initial_conditions = [0, 1]
+l= [0, 0, 1]
+n= [1, 2, 2]
+m_e=0.51099895069 
+a= 1/137
+a_0 = 1/(m_e*a)
+r = [np.linspace(1E-7*a_0, 2000, 1000), np.linspace(1E-7*a_0, 4200, 1000), np.linspace(1E-7*a_0, 4250, 1000)]
+u, v, u1, v1, u2, v2, u_2, u_3, u_4 = np.zeros((9,3,len(r[0])))
+
+for i in range(0,3):
+    E1, E2, E3 = CalculateInitialEnergy(n[i]) 
+    while abs(E3 - E1) > 1E-15:
+        u[i,:], v[i,:], u1[i,:], v1[i,:], u2[i,:], v2[i,:] = Solve(initial_conditions, r[i], l[i], m_e, E1, E2, E3, a, n[i])
+        Node1 = (CalculateNodes(u[i,:], v[i,:]))
+        Node2 = (CalculateNodes(u1[i,:], v1[i,:]))
+        Node3 = (CalculateNodes(u2[i,:], v2[i,:]))
+        Count1 = (CalculateTurningPoints(v[i,:]))
+        Count2 = (CalculateTurningPoints(v1[i,:]))
+        Count3 = (CalculateTurningPoints(v2[i,:]))
+        E1, E2, E3 = NewEnergy(E1, E2, E3, Node1, Node2, Node3, Count1, Count2, Count3)
+    print(E2)
+    u_2[i,:] = normalisation(u[i,:], r[i])
+    u_3[i,:] = normalisation(u1[i,:], r[i])
+    u_4[i,:] = normalisation(u2[i,:], r[i])
+    #print(u_3[i,:])
+    
+u_analytic, v_analytic, a_1, u_ratio = np.zeros((4, 3, len(r[0])))
+for i in range(0,3):
+    E = -1.36E-5/n[i]**2
+    analytic = odeint(system, initial_conditions, r[i], args=(l[i], m_e, E, a, n[i]))
+    u_analytic[i,:], v_analytic[i,:] = analytic.T 
+    a_1[i,:] = normalisation(u_analytic[i,:], r[i])
+    #u_ratio[i,:] = (u_3[i,:]/a_1[i,:])
+
+plt.figure(figsize=(10, 5))
+#print(a_1[0])
+plt.plot(r[0]/a_0, u_3[0], label='Numerical (1,0)', color='blue')
+plt.plot(r[1]/a_0, u_3[1], label='Numerical (2,0)', color='orange')
+plt.plot(r[2]/a_0, u_3[2], label='Numerical (2,1)', color='red')
+plt.plot(r[0]/a_0, a_1[0], '--', label='Analytic (1,0)', color='green')
+plt.plot(r[1]/a_0, a_1[1], '--', label='Analytic (2,0)', color='purple')
+plt.plot(r[2]/a_0, a_1[2], '--', label='Analytic (2,1)', color='brown')
+#plt.plot(r/a_0, u_ratio[0], '--', label='Analytic n=2,l=1', color='brown')
+#plt.plot(r/a_0, u_ratio[1], '--', label='Analytic n=2,l=1', color='brown')
+#plt.plot(r/a_0, u_ratio[2], '--', label='Analytic n=2,l=1', color='brown')
+plt.legend()
+plt.xlabel(r'$\frac{r}{a_0}$')
+plt.ylabel(r'$|U_{nl}(r)|^{2}$')
+
+plt.savefig("Truncation.png", dpi=300, bbox_inches='tight')
+
+plt.show()
+

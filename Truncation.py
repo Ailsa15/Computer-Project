82a797951eefd7d30c74bee5774679db837f4cf5
Ailsa15 <ailsa.scott0905@gmail.com>
2025-11-25 09:41:36 +0000

    Update Truncation.py

--- Truncation.py
+++ Truncation.py
@@ -54,13 +54,10 @@ def normalisation(u, r):
     u_squared = u_normalised**2
     return u_squared
 
-def round_sig(x, sig=3):
-    return float(f"{x:.{sig}g}")
-
 def round_sig_fig(x, sig=3):
     from math import log10, floor
     if x == 0:
-        return f"{0:.{sig-1}f}"
+        return f"0.{(sig-1)*'0'}"
     dec = sig - 1 - floor(log10(abs(x)))
     return f"{x:.{max(dec,0)}f}"
     
@@ -72,10 +69,11 @@ m_e = 0.511
 #a= 1/137
 a = 7.30E-3
 a_0 = 1/(m_e*a)
-r = [np.linspace(1E-7*a_0, 2000, 1000), np.linspace(1E-7*a_0, 4200, 1000), np.linspace(1E-7*a_0, 4250, 1000)]
+r = [np.linspace(1E-7*a_0, 2000, 10000), np.linspace(1E-7*a_0, 10000, 10000), np.linspace(1E-7*a_0, 10000, 10000)]
+r1 = [np.linspace(1E-7*a_0, 2000, 10000), np.linspace(1E-7*a_0, 4000, 10000), np.linspace(1E-7*a_0, 4250, 10000)]
 u, v, u1, v1, u2, v2, u_2, u_3, u_4 = np.zeros((9,3,len(r[0])))
 
-Energy = np.zeros((3))
+Energy = np.zeros(3, dtype=object)
 for i in range(0,3):
     E1, E2, E3 = CalculateInitialEnergy(n[i]) 
     while abs(E3 - E1) > 1E-15:
@@ -87,32 +85,40 @@ for i in range(0,3):
         Count2 = (CalculateTurningPoints(v1[i,:]))
         Count3 = (CalculateTurningPoints(v2[i,:]))
         E1, E2, E3 = NewEnergy(E1, E2, E3, Node1, Node2, Node3, Count1, Count2, Count3)
-    E2 = round_sig(E2, sig=3)
-    Energy[i] = E2*1E6 
+    E2 = E2*1E6
+    E2 = round_sig_fig(E2, sig=3)
+    Energy[i] = E2
     u_2[i,:] = normalisation(u[i,:], r[i])
     u_3[i,:] = normalisation(u1[i,:], r[i])
     u_4[i,:] = normalisation(u2[i,:], r[i])
 
 Energya = np.zeros(3, dtype=object)
-u_analytic, v_analytic, a_1, u_ratio = np.zeros((4, 3, len(r[0])))
+u_analytic, v_analytic, a_1, u_ratio = np.zeros((4, 3, len(r1[0])))
 for i in range(0,3):
     E = -1.36E-5/n[i]**2
-    analytic = odeint(system, initial_conditions, r[i], args=(l[i], m_e, E, a, n[i]))
+    analytic = odeint(system, initial_conditions, r1[i], args=(l[i], m_e, E, a, n[i]))
     u_analytic[i,:], v_analytic[i,:] = analytic.T 
-    a_1[i,:] = normalisation(u_analytic[i,:], r[i])
+    a_1[i,:] = normalisation(u_analytic[i,:], r1[i])
     E = E*1E6
     E = round_sig_fig(E, sig=3)
     Energya[i] = E
     #u_ratio[i,:] = (u_3[i,:]/a_1[i,:])
+
 print(Energy)
 print(Energya)
+u_31_trunc = u_3[1, :4000]
+u_32_trunc = u_3[2, :4250]
+r_trunc1 = r[1][:4000]    
+r_trunc2 = r[2][:4250]
+
+
 plt.figure(figsize=(10, 5))
 plt.plot(r[0]/a_0, u_3[0], label=f'Numerical (1,0) Energy = {Energy[0]} eV', color='blue')
-plt.plot(r[1]/a_0, u_3[1], label=f'Numerical (2,0) Energy = {Energy[1]} eV', color='orange')
-plt.plot(r[2]/a_0, u_3[2], label=f'Numerical (2,1) Energy = {Energy[2]} eV', color='red')
-plt.plot(r[0]/a_0, a_1[0], '--', label=f'Analytic (1,0) Energy = {Energya[0]} eV', color='green')
-plt.plot(r[1]/a_0, a_1[1], '--', label=f'Analytic (2,0) Energy = {Energya[1]} eV', color='purple')
-plt.plot(r[2]/a_0, a_1[2], '--', label=f'Analytic (2,1) Energy = {Energya[2]} eV', color='brown')
+plt.plot(r_trunc1/a_0, u_31_trunc, label=f'Numerical (2,0) Energy = {Energy[1]} eV', color='orange')
+plt.plot(r_trunc2/a_0, u_32_trunc, label=f'Numerical (2,1) Energy = {Energy[2]} eV', color='red')
+plt.plot(r1[0]/a_0, a_1[0], '--', label=f'Analytic (1,0) Energy = {Energya[0]} eV', color='green')
+plt.plot(r1[1]/a_0, a_1[1], '--', label=f'Analytic (2,0) Energy = {Energya[1]} eV', color='purple')
+plt.plot(r1[2]/a_0, a_1[2], '--', label=f'Analytic (2,1) Energy = {Energya[2]} eV', color='brown')
 #plt.plot(r/a_0, u_ratio[0], '--', label='Analytic n=2,l=1', color='brown')
 #plt.plot(r/a_0, u_ratio[1], '--', label='Analytic n=2,l=1', color='brown')
 #plt.plot(r/a_0, u_ratio[2], '--', label='Analytic n=2,l=1', color='brown')

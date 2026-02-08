769e3fc0a26a84e133c8e40dbcc4f742b56bacd9
Ailsa15 <ailsa.scott0905@gmail.com>
2026-02-08 11:49:12 +0000

    Create Values for Charmonium.py

---
+++ Values for Charmonium.py
@@ -0,0 +1,19 @@
+#Beta values: Doesn't work as trying to minimise - can argue why to use ground state as lowest energy and use non-relativistic quantum mechanics
+1S = 0.19078229503268146
+2S = 0.19074871857469727
+3S = 0.18694354365057125
+4S = 0.14824497226351752
+
+#Just used beta from 1S value
+#Energy Levels (GeV):
+1S = 3.0654999265342577
+2S = 3.692384545109543
+3S = 4.162731332048331
+
+#DeltaE Values
+1S = 0.1329181133959173
+2S = 0.09644712130509946
+3S = 0.08566453598601087
+
+#Percentage difference between experimental and calculated delta energy:
+1S = 
